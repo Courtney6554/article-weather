c8c56192516d14e15f8ecf478efa2294551568ef
Dayton Mills <dayton@statscraft.net>
2017-09-06 13:24:35 -0700

    Starting UI

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,34 +1,39 @@
 //New York Times
 var dataForWeather=[];
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-url += '?' + $.param({
-  'api-key': "d4a45b6e145a424ba8a4730581bc76a6",
-  'q': "Trump",
-  'fl': "web_url,snippet,multimedia,headline,byline,keywords,pub_date"
-});
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  //For each article from query
-  for(i = 0; i < result.response.docs.length; i++)
-  {
-      //For each keyword in that article metadata
-      for(k = 0; k < result.response.docs[i].keywords.length; k++)
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+    let query = $("#searchField").val();
+    url += '?' + $.param({
+      'api-key': "d4a45b6e145a424ba8a4730581bc76a6",
+      'q': query,
+      'fl': "web_url,snippet,multimedia,headline,byline,keywords,pub_date"
+    });
+
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      //For each article from query
+      for(i = 0; i < result.response.docs.length; i++)
       {
-          //If there are any keywords containing location data
-          if(result.response.docs[i].keywords[k].name === "glocations")
+          //For each keyword in that article metadata
+          for(k = 0; k < result.response.docs[i].keywords.length; k++)
           {
-              //Convert data to a validated address
-              date = result.response.docs[i].pub_date;
-              getAddress(result.response.docs[i].keywords[k].value, date);
+              //If there are any keywords containing location data
+              if(result.response.docs[i].keywords[k].name === "glocations")
+              {
+                  //Convert data to a validated address
+                  date = result.response.docs[i].pub_date;
+                  getAddress(result.response.docs[i].keywords[k].value, date);
+              }
           }
       }
-  }
-}).fail(function(err) {
-  throw err;
-});
+    }).fail(function(err) {
+      throw err;
+    });
+})
 
 //Address Validation
 function getAddress(value, date) {
