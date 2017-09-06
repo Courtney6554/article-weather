934c3609ebd7e66787c9301e0396a0207cbd303a
Dayton Mills <dayton@statscraft.net>
2017-09-06 14:46:05 -0700

    Article Object and reformatting

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,12 +1,29 @@
-//New York Times
-var dataForWeather=[];
+//Initialize variables
+var articles=[];
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+var dateStart = "20170801";
+var dateEnd = "20170901";
 
+//Initialize Date range selection
+$(function() {
+    $('#daterange').daterangepicker();
+});
+
+//Apply Date range selection
+$('#daterange').on('apply.daterangepicker', function(ev, picker) {
+    dateStart = picker.startDate.format('YYYYMMDD').toString();
+    dateEnd = picker.endDate.format('YYYYMMDD').toString();
+});
+
+//Query New York Times API with our data
 $("#searchBtn").on("click", function(event){
+    console.log(dateStart + " " + dateEnd);
     event.preventDefault();
     let query = $("#searchField").val();
     url += '?' + $.param({
       'api-key': "d4a45b6e145a424ba8a4730581bc76a6",
+      'begin_date': dateStart,
+      'end_date': dateEnd,
       'q': query,
       'fl': "web_url,snippet,multimedia,headline,byline,keywords,pub_date"
     });
@@ -21,12 +38,20 @@ $("#searchBtn").on("click", function(event){
           //For each keyword in that article metadata
           for(k = 0; k < result.response.docs[i].keywords.length; k++)
           {
-              //If there are any keywords containing location data
+              //Only get articles containing location data
               if(result.response.docs[i].keywords[k].name === "glocations")
               {
-                  //Convert data to a validated address
-                  date = result.response.docs[i].pub_date;
-                  getAddress(result.response.docs[i].keywords[k].value, date);
+                  //Create article data
+                  var data = result.response.docs[i];
+                  var article = {
+                      headline: data.headline.main,
+                      snippet: data.snippet,
+                      date: data.pub_date,
+                      address: data.keywords[k].value
+                  }
+                  verifyAddress(article);
+                  articles.push(article);
+                  console.log(articles);
               }
           }
       }
@@ -36,23 +61,18 @@ $("#searchBtn").on("click", function(event){
 })
 
 //Address Validation
-function getAddress(value, date) {
-    var CurrentAddress = value;
-    CurrentAddress = CurrentAddress.replace(/\n/g, "");
+function verifyAddress(article) {
+    var currentAddress = article.address;
+    currentAddress = currentAddress.replace(/\n/g, "");
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({
-        'address': CurrentAddress
+        'address': currentAddress
         }, function (results, status) {
 
-        if (status == google.maps.GeocoderStatus.OK) {
+        if (status === google.maps.GeocoderStatus.OK)
+        {
             address = results[0].formatted_address;
-            numCommas = address.match(/,/g).length;
-            var obj = {
-                address: address,
-                date: date
-            };
-            dataForWeather.push(obj);
-            console.log(dataForWeather);
+            article.address = address;
         }
 	});
 }
